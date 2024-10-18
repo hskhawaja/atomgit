2f659a6480e687240e8ce41b5c90905eb7bcc840
Hussain Shahbaz Khawaja <hskhawaja@live.com>
2024-10-18 19:44:49 +0500

    uploading the debug file to track changes

---
+++ debug.py
@@ -0,0 +1,53 @@
+import random
+from parsers.pdfparser import PDFParser
+
+def load_doc(file):
+    """
+    Test for error thrown by PDFParser
+    :param file: .pdf file
+    :return:
+    """
+    document = PDFParser(file)
+    toc = document.toc.toc_dict
+    sections = document.TextParser.Sectioning.sections_dict
+
+    return document, toc, sections
+
+def test_sectioning(file):
+    """
+    Test for sectioning error (len sections not corresponding to len toc)
+    :param file: .pdf file
+    :return:
+    """
+    document, toc, sections = load_doc(file)
+    if len(toc) >= len(sections.keys()):
+        raise Exception('Sectioning fails to catch all toc items.')
+
+    return document,toc,sections
+
+def test_preamble(file):
+    """
+    Test for preamble creation
+    :param file: .pdf file
+    :return:
+    """
+    document, toc, sections = load_doc(file)
+    preamble = document.TextParser.get_toc_header(0).text()
+    print(preamble)
+
+    return preamble
+
+
+if __name__ == "__main__":
+    path = "/home/david/PycharmProjects/frq-nlp/Data/prospectus"
+    files_parse_error = ["Candriam.pdf", "pictet_sicav.pdf", "pictet_multi.pdf"]
+    
+
+    ##1 Parsing error
+    #document,toc,sections = load_doc(path + "/" + random.choice(files_parse_error))
+
+    ##2 Sectioning error
+    document,toc,sections = test_sectioning(path + "/" + random.choice(files_section_error))
+
+    ##3 Preamble error
+    preamble = test_preamble(path + "/" + random.choice(files_preamble_error))
